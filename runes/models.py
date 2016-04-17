f5ecdd9715ab5e81b838d0fd2f5d13d645a567ef
unknown <chazly321@gmail.com>
2016-04-17 11:00:05 -0700

    fixed models for runes..

--- runes/models.py
+++ runes/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Rune(models.Model):
 
     runeId = models.IntegerField(blank = False, null = True)
-    description = models.CharField(max_length = 200)
-    name = models.SlugField()
+    description = models.CharField(max_length = 2000)
+    name = models.SlugField(max_length = 200)
 
     def __unicode__(self):
         return unicode(self.name)
