97559899c92e8276f25ad7d56d19be7eb7d0b1af
unknown <chazly321@gmail.com>
2016-04-17 11:30:44 -0700

    added model changes

--- masteries/models.py
+++ masteries/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Mastery(models.Model):
 
     masteryId = models.IntegerField(blank = False, null = True)
-    name = models.SlugField()
-    description = models.CharField(max_length = 200)
+    description = models.CharField(max_length = 10000)
+    name = models.SlugField(max_length = 1000)
 
     def __unicode__(self):
         return unicode(self.name)
